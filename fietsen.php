d8a00abac39cdd9313e5fd45064e6c3a18911da3
TJ <nathtj250@gmail.com>
2019-11-18 13:18:05 +0545

    my first commit

---
+++ fietsen.php
@@ -0,0 +1,151 @@
+<?php 
+	require_once "Layout/header.php"
+?>
+<section class="container-fluid">
+			<div class="fietsen_bgimg">
+				<h4> ROAD</h4> 
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,<br> sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+</section>
+
+<section class="container-fluid ml-15 mr-15">
+		<div class="row ml-0 mr-0"  style="overflow: hidden;">
+			<div class="col-x8-6 col-lg-8 col-md-8 pt-4 pb-4 ">
+				<P class="text-dark"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident,  
+				</P>
+			</div>
+
+			<div class="col-xl-4 col-lg-4 col-md-4" >
+				<img class="img-responsive fiet_image pt-4 pb-4" src="Images/fietsen1.jpg" style="background-size:cover; width: 110%;">
+			</div>
+		</div>
+	
+		<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-2 col-md-2 col-sm-2 d-inline-block text-white text-center ab" style="background-color: black;">Alles</div>
+			<div class="col-lg-2 col-md-2 col-sm-2 d-inline-block text-white bg-dark text-center ab">Performance</div>
+			<div class="col-lg-2 col-md-2 col-sm-2 d-inline-block text-white bg-dark text-center ab">Adventure & Gravel</div>
+			<div class="col-lg-2 col-md-2 col-sm-2 d-inline-block text-white bg-dark text-center ab">Cyclocross</div>
+			<div class="col-lg-2 col-md-2 col-sm-2 d-inline-block text-white bg-dark text-center ab">Triathlon</div>
+			<div class="col-lg-2 col-md-2 col-sm-2 d-inline-block text-white bg-dark text-center ab">Frames</div>
+		</div>
+	</div>
+		<div class="container-fluid m-3">
+			<div class="row">
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen21.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">S-Works Roubaix</p>
+					<p class="text-center">10 499,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen22.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Allez Sports</p>
+					<p class="text-center">1 049,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen23.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Men's S-Works Termac Disc</p>
+					<p class="text-center">10 799,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen24.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">S-Works Roubaix</p>
+					<p class="text-center">10 499,00 <span>&#8364;</span></p>
+
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen25.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Allez Sports</p>
+					<p class="text-center">1 049,00 <span>&#8364;</span></p>
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen26.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Ruby Comp</p>
+					<p class="text-center">3 699,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen27.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">S-Works Roubaix</p>
+					<p class="text-center">10 499,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen28.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Allez Sports</p>
+					<p class="text-center">1 049,00 <span>&#8364;</span></p>
+
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen221.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">S-Works Roubaix</p>
+					<p class="text-center">10 499,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen222.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Allez Sports</p>
+					<p class="text-center">1 049,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen223.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Men's S-Works Termac Disc</p>
+					<p class="text-center">10 799,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen224.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">S-Works Roubaix</p>
+					<p class="text-center">10 499,00 <span>&#8364;</span></p>
+
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen225.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Allez Sports</p>
+					<p class="text-center">1 049,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen226.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Ruby Comp</p>
+					<p class="text-center">3 699,00 <span>&#8364;</span></p>
+					
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen227.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Allez Sports</p>
+					<p class="text-center">1 049,00 <span>&#8364;</span></p>
+
+				</div>
+				<div class="col-md-3">
+					<a href="#"><img class="img-responsive bike_img" src="Images/fietsen228.jpg"></a>
+					<p class="text-center font-weight-bold mt-3">Allez Sports</p>
+					<p class="text-center">1 049,00 <span>&#8364;</span></p>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+<?php require_once "Layout/footer.php";?>
